e1c2f09894b3369eb0bee9baeb0885067adddf96
olaf0623-crypto <olaf0623@g.shingu.ac.kr>
2026-04-17 22:57:47 +0900

    UI complete

---
+++ My project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+
+public class Playercontroller : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+
+    private Rigidbody2D rb;
+    private Animator pAni;
+    private bool isGrounded;
+    private float moveInput;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        pAni = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+
+    public void OnMove(InputValue value)
+    {
+        Vector2 input = value.Get<Vector2>();
+        moveInput = input.x;
+    }
+
+
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            pAni.SetTrigger("Jump");
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Respawn"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
+
